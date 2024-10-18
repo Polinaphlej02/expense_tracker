ac6fb5a3a6460eda689a94736c527dd8454b5265
Polina <phlejnas009@mail.ru>
2024-10-19 00:06:36 +0300

    Maded some dummy data

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'models/expense.dart';
+
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
 
@@ -8,13 +10,32 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: "Laptop",
+      amount: 20.0,
+      category: Category.work,
+      date: DateTime.now(),
+    ),
+    Expense(
+      title: "Egypt Tour",
+      amount: 700.0,
+      category: Category.travel,
+      date: DateTime.now(),
+    ),
+    Expense(
+      title: "Ice cream",
+      amount: 3.0,
+      category: Category.food,
+      date: DateTime.now(),
+    )
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: [
-          
-        ],
+        children: [],
       ),
     );
   }
