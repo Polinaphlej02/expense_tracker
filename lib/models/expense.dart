6d19515792d36383cb1496807bdb9a91d9bcecfc
Polina <phlejnas009@mail.ru>
2024-10-21 02:37:28 +0300

    Added expense bucket constructure

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -21,9 +21,12 @@ const categoryIcons = {
 };
 
 class Expense {
-  Expense({
-    required this.title, required this.amount, required this.date, required this.category
-  }) : id = uuid.v4(); // generate unique String
+  Expense(
+      {required this.title,
+      required this.amount,
+      required this.date,
+      required this.category})
+      : id = uuid.v4(); // generate unique String
 
   final String id;
   final String title;
@@ -36,3 +39,23 @@ class Expense {
   }
 }
 
+class ExpenseBucket {
+  const ExpenseBucket({required this.category, required this.expenses});
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+
+    return sum;
+  }
+}
