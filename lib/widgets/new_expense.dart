03c36ddacbb0bf7a75265bed8173a48ff898e4f8
Polina <phlejnas009@mail.ru>
2024-10-21 00:12:12 +0300

    Fixed modal bottom sheet

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -9,33 +9,73 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+    
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-
-    final _titleController = TextEditingController();
-    
-    @override
-    void dispose() {
-      _titleController.dispose();
-      super.dispose();
-    }
 
     return Padding(
       padding: EdgeInsets.all(16),
       child: Column(
         children: [
-          TextField(
-            controller: _titleController,
-            maxLength: 50,
-            decoration: InputDecoration(
-              label: Text("Title"),
-            ),
+             TextField(
+              controller: _titleController,
+              maxLength: 50,
+              decoration: InputDecoration(
+                label: Text("Title"),
+              ),
           ),
           Row(
             children: [
+              Expanded(
+                child: TextField(
+                  controller: _amountController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    prefixText: '\$ ',
+                    label: Text("Amount"),
+                  ),
+                ),
+              ),
+              SizedBox(width: 16,),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Text("Date"),
+                    IconButton(
+                      onPressed: () {
+
+                      }, 
+                      icon: Icon(Icons.calendar_month))
+                  ],
+                  
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text("Cancel"),
+                ),
               ElevatedButton(
                  onPressed: () {
                    print(_titleController.text);
+                   print(_amountController.text);
                 },
                 child: const Text("Save"),
                 )
