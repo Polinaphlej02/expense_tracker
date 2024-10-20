94324a4b12f1bc1a37a604d8a9556744bf43741e
Polina <phlejnas009@mail.ru>
2024-10-21 00:59:15 +0300

    Added validation and alert dialog

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -21,6 +21,30 @@ class _NewExpenseState extends State<NewExpense> {
     super.dispose();
   }
 
+  void _submitExpenseData() {
+    final enteredAmount = double.tryParse(_amountController.text);
+    final amountIsInvalid = enteredAmount == null || enteredAmount <= 0;
+    if (_titleController.text.trim().isEmpty ||
+        amountIsInvalid ||
+        _selectedDate == null) {
+      showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+          title: Text("Invalid input"),
+          content: const Text(
+              "Please make sure a valid title, amount, date and category was entered"),
+          actions: [
+            TextButton(
+                onPressed: () {
+                  Navigator.pop(ctx);
+                },
+                child: Text("Okey"))
+          ],
+        ),
+      );
+    }
+  }
+
   void _datePicker() async {
     final now = DateTime.now();
     final firstDate = DateTime(now.year - 1, now.month, now.day);
@@ -79,19 +103,19 @@ class _NewExpenseState extends State<NewExpense> {
               ),
             ],
           ),
-          const SizedBox(height: 16,),
+          const SizedBox(
+            height: 16,
+          ),
           Row(
             children: [
               DropdownButton(
                 value: _selectedCategory,
                 items: Category.values
-                    .map((category) =>
-                        DropdownMenuItem(
-                          value: category,
-                          child: Text(
-                            category.name.toUpperCase(),
-                            )
-                          ))
+                    .map((category) => DropdownMenuItem(
+                        value: category,
+                        child: Text(
+                          category.name.toUpperCase(),
+                        )))
                     .toList(),
                 onChanged: (value) {
                   if (value == null) {
@@ -111,8 +135,7 @@ class _NewExpenseState extends State<NewExpense> {
               ),
               ElevatedButton(
                 onPressed: () {
-                  print(_titleController.text);
-                  print(_amountController.text);
+                  _submitExpenseData();
                 },
                 child: const Text("Save"),
               )
